ceeb46a76d0009f366e5c209ade3c8c677ac3cd5
prathamdalmia <prathamdalmia2003@gmail.com>
2026-02-26 17:48:04 +0530

    Extend JWT token expiration to 30 days

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (userId, type = "student") => {
-  return jwt.sign({ id: userId, type }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id: userId, type }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
